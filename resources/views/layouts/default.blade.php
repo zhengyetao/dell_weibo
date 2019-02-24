43c0e33785a00b748fc26ee9dad06979bcfd8dff
zhengyetao <zhengyetao@126.com>
2019-02-24 06:29:01 +0000

    用户注册页面

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,8 +8,10 @@
   @include('layouts._header')
 
   <div class="container">
-    @yield('content')
-    @include('layouts._footer')
+    <div class="offset-md-1 col-md-10">
+      @yield('content')
+      @include('layouts._footer')
+    </div>
   </div>
 </body>
 </html>
