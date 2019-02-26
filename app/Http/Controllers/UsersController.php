799be1a86d5df19495c851a0e0b174b213c2c89d
zhengyetao <zhengyetao@126.com>
2019-02-26 16:03:41 +0000

    中文的错误提示信息

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -19,6 +19,10 @@ class UsersController extends Controller
 
     public function store(Request $request)
     {
-        
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
     }
 }
